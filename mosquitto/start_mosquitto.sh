768bdf3d344452130b6467c4e6e87db9970adf47
Colin McNaughton <c.a.mcnaughton@gmail.com>
2024-05-25 11:50:33 -0400

    env vars for mqtt bridge

---
+++ mosquitto/start_mosquitto.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Set default values for environment variables if not provided
+: ${REMOTE_ADDRESS:=default_address:1883}
+: ${REMOTE_USERNAME:=default_username}
+: ${REMOTE_PASSWORD:=default_password}
+
+# Substitute environment variables in the template configuration file
+envsubst < /mosquitto/config/mosquitto.conf.template > /mosquitto/config/mosquitto.conf
+
+# Start mosquitto with the generated configuration file
+mosquitto -c /mosquitto/config/mosquitto.conf
