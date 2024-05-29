7c79785c43d624b20435209ecbe1a8b5e22701ce
Colin <c.a.mcnaughton@gmail.com>
2024-05-29 15:57:17 -0400

    quote strings

--- mosquitto/start_mosquitto.sh
+++ mosquitto/start_mosquitto.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Set default values for environment variables if not provided
-: ${REMOTE_ADDRESS:=default_address:1883}
-: ${REMOTE_USERNAME:=default_username}
-: ${REMOTE_PASSWORD:=default_password}
+: ${REMOTE_ADDRESS:='default_address:1883'}
+: ${REMOTE_USERNAME:='default_username'}
+: ${REMOTE_PASSWORD:='default_password'}
 
 # Substitute environment variables in the template configuration file
 envsubst < /mosquitto/config/mosquitto.conf.template > /mosquitto/config/mosquitto.conf
